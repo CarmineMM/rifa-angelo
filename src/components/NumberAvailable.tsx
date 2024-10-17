c31e77bfb49e7d0f425211e7c7251913c3b261c4
Carmine Maggio <carminemaggiom@gmail.com>
2024-10-17 14:46:18 -0400

    feat: new multiselect numbers

--- src/components/NumberAvailable.tsx
+++ src/components/NumberAvailable.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef } from 'react'
+import React, { useCallback, useMemo, useRef, useState } from 'react'
 import { type NumberUsed } from './WantParticipate'
 
 interface Props {
@@ -8,6 +8,12 @@ interface Props {
 const NumberAvailable = ({ numbersUsed }: Props) => {
     const modalRef = useRef<HTMLDialogElement | null>(null)
     const alreadyUsed = useMemo(() => numbersUsed.map(item => item.num) as number[], [numbersUsed])
+    const [preSelectedNumbers, setSelectedNumbers] = useState<number[]>([])
+    const [feedback, setFeedback] = useState({
+        className: '',
+        message: 'Selecciona los números a adquirir en la rifa',
+        loading: false,
+    });
 
     /**
      * Click sobre el botón, para ver números disponibles
@@ -27,8 +33,30 @@ const NumberAvailable = ({ numbersUsed }: Props) => {
             return alert('El numero ya fue usado')
         }
 
-        window.open(`https://wa.me/+584124413347?text=Compre el Numero *${num}* de la rifa de Angelo Camacho`, '_blank')
-    }, [alreadyUsed])
+        // window.open(`https://wa.me/+584124413347?text=Compre el Numero *${num}* de la rifa de Angelo Camacho`, '_blank')
+        if (preSelectedNumbers.includes(num)) {
+            setSelectedNumbers(prev => prev.filter(item => item !== num))
+        } else {
+            setSelectedNumbers(prev => [...prev, num])
+        }
+    }, [alreadyUsed, preSelectedNumbers])
+
+    /**
+     * Procesar los números
+     */
+    const processNumbers = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        setFeedback({
+            className: 'alert-info',
+            message: 'Adjunte la referencia de pago en el WhatsApp',
+            loading: true,
+        })
+
+        setTimeout(() => {
+            window.open(`https://wa.me/+584124413347?text=Compre los Números *${preSelectedNumbers.join(', ')}* de la rifa de Angelo Camacho, para la *Limpieza Dental con la OD Dayana Camacho*`, '_blank');
+        }, 700)
+    }, [preSelectedNumbers])
 
     return (
         <>
@@ -40,7 +68,11 @@ const NumberAvailable = ({ numbersUsed }: Props) => {
                         {Array.from({ length: 100 }, (_, i) => i).map((num) => (
                             <li key={num}>
                                 <button
-                                    className={`hover:bg-base-300 transition cursor-pointer rounded px-2 py-1 ${alreadyUsed.includes(num) ? 'bg-success-content hover:bg-base-content text-base-200 tooltip' : ''}`}
+                                    className={`
+                                        transition cursor-pointer rounded px-2 py-1 
+                                        ${alreadyUsed.includes(num) ? 'bg-success-content hover:bg-base-content text-base-200 tooltip' : ''}
+                                        ${preSelectedNumbers.includes(num) ? 'bg-primary text-base-100' : 'hover:bg-base-300 '}
+                                    `}
                                     onClick={(e) => handleClickNum(e, num)}
                                     data-tip={`Ocupado por: ${numbersUsed.find(item => item.num === num)?.name}`}
                                     type='button'
@@ -48,7 +80,7 @@ const NumberAvailable = ({ numbersUsed }: Props) => {
                             </li>
                         ))}
                     </ul>
-                    <div role="alert" className="alert mt-3 justify-center">
+                    <div role="alert" className={`alert mt-3 justify-center ${feedback.className}`}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -60,15 +92,19 @@ const NumberAvailable = ({ numbersUsed }: Props) => {
                                 strokeWidth="2"
                                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
-                        Selecciona uno para registrarte en la Rifa
+                        <p>{feedback.message}</p>
                     </div>
                     <div className="modal-action">
+                        {setSelectedNumbers.length > 0 && (
+                            <button className='btn btn-primary' onClick={processNumbers}>Procesar Números</button>
+                        )}
                         <button
                             className="btn"
                             type='button'
                             onClick={(e) => {
                                 e.preventDefault()
                                 modalRef.current?.close()
+                                setSelectedNumbers([])
                             }}
                         >Cerrar</button>
                     </div>
