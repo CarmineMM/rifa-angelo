184c6c40591713dace12e54f934b3262b2b47430
Carmine Maggio <carminemaggiom@gmail.com>
2024-10-17 12:56:17 -0400

    feat: integrate .env file

--- src/components/Acknowledgements.tsx
+++ src/components/Acknowledgements.tsx
@@ -5,10 +5,11 @@ const Acknowledgements = () => {
         <div className='absolute bottom-7 md:bottom-3 text-neutral text-center'>
             <p>¡Gracias de antemano por tu apoyo!</p>
             <p className='text-xs text-success flex gap-1 items-center'>
-                <p>Agradecimientos a</p>
+                <span>Agradecimientos a</span>
                 <a className='underline flex' href="https://zeabur.com?referralCode=CarmineMM" target='_blank'><img src={ZeaburLogo} alt="" className='w-15 h-3' /></a>
-                <p> y </p>
-                <a href="https://github.com/" target='_blank' className='underline'>Carmine Maggio</a></p>
+                <span> y </span>
+                <a href="https://github.com/" target='_blank' className='underline'>Carmine Maggio</a>
+            </p>
         </div>
     )
 }
--- src/database/Firebase.ts
+++ src/database/Firebase.ts
@@ -1,18 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+const {
+    VITE_API_KEY: FIREBASE_API_KEY,
+    VITE_AUTH_DOMAIN: FIREBASE_AUTH_DOMAIN,
+    VITE_PROJECT_ID: FIREBASE_PROJECT_ID,
+    VITE_STORAGE_BUCKET: FIREBASE_STORAGE_BUCKET,
+    VITE_MESSAGING_SENDER_ID: FIREBASE_MESSAGING_SENDER_ID,
+    VITE_APP_ID: FIREBASE_APP_ID,
+    VITE_MEASUREMENT_ID: FIREBASE_MEASUREMENT_ID,
+    VITE_DATABASE_URL: FIREBASE_DATABASE_URL
+} = import.meta.env
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAtYLPbeEGTLcl0hXJOHkCxLHUJQbi-6kE",
-    authDomain: "rifa-f4e3e.firebaseapp.com",
-    projectId: "rifa-f4e3e",
-    storageBucket: "rifa-f4e3e.appspot.com",
-    messagingSenderId: "1020166494803",
-    appId: "1:1020166494803:web:75df0e1e28e49fb5c004a6",
-    measurementId: "G-K3DG3GV039",
-    databaseURL: "https://rifa-f4e3e-default-rtdb.firebaseio.com/"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID,
+    databaseURL: FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
